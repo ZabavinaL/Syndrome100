1bccc8852d2e4a88f63da9916ce0f036ab7814b0
Lyubov Zabavina <zabavinal@yandex.ru>
2021-01-23 15:11:49 +0300

    pom.xml edited

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -8,7 +8,7 @@ public class StatisticsService {
      * @return - index of first max value
      */
     public long findMax(long[] incomes) {
-//        long current_max_index = 0;
+        long current_max_index = 0;
         long current_max = incomes[0];
 
         for (long income : incomes) {
